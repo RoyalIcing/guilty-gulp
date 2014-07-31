dabbe19ace2b8a5c1ed0249f649fe3ca8c4a39d5
Patrick Smith <pgwsmith@gmail.com>
2014-07-31 22:22:43 +1000

    Improved compass src and dest path handling.

--- compass.js
+++ compass.js
@@ -2,35 +2,42 @@ var compass = require('gulp-compass');
 var prefix = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 
-module.exports = function compassTask(gulp, guilty)
+module.exports = function compassTask(gulp, guilty, filePath, destPath)
 {
+	if (filePath == null) {
+		filePath = 'main.scss';
+	}
+	
+	if (destPath == null) {
+		destPath = 'css/';
+	}
+	
 	gulp.task(guilty.taskName('compass'),
 		[
 			guilty.taskName('setup'),
 			guilty.taskName('images')
 		],
 		function() {
-			return gulp.src('./src/main.scss')
+			return gulp.src(guilty.srcPath(filePath))
 				.pipe(plumber({
 					errorHandler: function(error) {
 						console.log(error.message);
 			
-						notify.onError(function (error) {
+						/*notify.onError(function (error) {
 					        return "Message to the notifier: " + error.message;
-						})(error);
+						})(error);*/
 					}
 				}))
 				.pipe(compass({
 					//config_file: './config.rb',
-					sass: './src',
-					css: guilty.destPath('css'),
+					sass: guilty.srcPath(),
+					css: guilty.destPath(destPath),
 					image: guilty.destPath('images'),
 					javascript: guilty.destPath('js'),
 					font: guilty.destPath('font')
 				}))
 				.pipe(prefix('last 2 version', '> 1%', 'ie 8'))
-				.pipe(guilty.destCSS('/'))
-				//.pipe(gulp.dest(baseDestFolder + 'css/main/'))
+				.pipe(guilty.destCSS(destPath))
 			;
 		}
 	);
